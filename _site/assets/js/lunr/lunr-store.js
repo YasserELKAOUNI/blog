d5e4c29bfd9ed30a1c47ce93dddab8009083da9a
Your Name <yasserkaouni@gmail.com>
2024-11-07 13:19:02 +0100

    Initial commit: Configuration du nouveau blog avec styles SCSS

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Transformer Series: Introduction",
+        "excerpt":"J’entame une série d’articles sur l’architecture des transformers. Aujourd’hui, ce paradigme dominant en apprentissage profond est omniprésent dans tous les aspects structurant notre rapport à la technologie et à l’intelligence artificielle. Inventé en 2017 avec le papier “Attention Is All You Need”1, les transformers soutiennent les systèmes de recommandation des...","categories": ["deep-learning"],
+        "tags": ["Transformers","NLP","AI"],
+        "url": "/deep-learning/2024/09/27/transformer-series-introduction.html",
+        "teaser": null
+      }]
